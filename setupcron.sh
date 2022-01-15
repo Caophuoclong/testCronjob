9d6723418c78677efa2a5d83bd5493150e0f97b0
caophuoclong <caophuoclong1@gmail.com>
2022-01-15 10:03:56 +0700

    update

--- setupcron.sh
+++ setupcron.sh
@@ -1,6 +1,6 @@
 sudo apt update && sudo apt-get update
 sudo apt install tar s3cmd
-cp ./backup.sh ~/backups.sh
+cp ./backups.sh ~/backups.sh
 cp ./crontab ~/crontab
 sudo touch /var/log/backup.mongo.log
 sudo chown 999:999 /var/log/backup.mongo.log
